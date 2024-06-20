67b65c9d17053818a4e4c896e734e03b028743c1
Sergey Mkrtumyan <smkrtum@yandex.ru>
2024-06-20 15:50:55 +0400

    Remove ExecuteAsync

--- AzureAiMicroservice/Endpoints/AzureDevopsEndpoint.cs
+++ AzureAiMicroservice/Endpoints/AzureDevopsEndpoint.cs
@@ -10,12 +10,7 @@ public class AzureDevopsEndpoint(AzureDevopsService devopsService, JiraService j
         Post("/api/tasks");
         AllowAnonymous();
     }
-
-    public override Task<object?> ExecuteAsync(CancellationToken ct)
-    {
-        return base.ExecuteAsync(ct);
-    }
-
+    
     public override async Task HandleAsync(CancellationToken ct)
     {
         await jiraService.GetTasks();
